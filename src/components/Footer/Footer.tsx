5ed68e6f3d35607094bf8ee23f366607abc9fcf2
AleksandrZachkevich <aleksdark13@gmail.com>
2023-12-17 11:51:08 +0200

    add link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,11 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import {Grid, IconButton, Link} from "@mui/material";
+import {Grid, IconButton} from "@mui/material";
 import "./footer.scss"
 import {makeStyles} from "tss-react/mui";
+import GoogleIcon from '@mui/icons-material/Google';
+
 
 const useStyles = makeStyles()((theme) => ({
 
@@ -25,8 +27,28 @@ const Footer = () => {
     const {cx, classes} = useStyles();
 
 
-    let arrayIcon = [<FacebookIcon fontSize={"large"}/>, <InstagramIcon fontSize={"large"}/>,
-        <LinkedInIcon fontSize={"large"}/>, <TelegramIcon fontSize={"large"}/>]
+    let arrayIcon = [
+        {
+            icon: <FacebookIcon fontSize={"large"}/>,
+            href: "https://www.facebook.com/dragondark13"
+        },
+        {
+            icon: <InstagramIcon fontSize={"large"}/>,
+            href: "https://www.instagram.com/zachkevich/"
+        },
+        {
+            icon: <LinkedInIcon fontSize={"large"}/>,
+            href: "https://www.linkedin.com/in/alexandrzachkevich/"
+        },
+        {
+            icon: <TelegramIcon fontSize={"large"}/>,
+            href: "https://t.me/AlexandrZachkevich"
+        },
+        {
+            icon: <GoogleIcon fontSize={"large"}/>,
+            href: "mailto:aleksdark13@gmail.com"
+        }
+        ]
 
     return (
         <footer className={cx(classes.footer, "footer")}>
@@ -34,9 +56,9 @@ const Footer = () => {
                 <Grid justifyContent={"center"} container spacing={2}>
 
 
-                    {arrayIcon.map((icon, index) => (
+                    {arrayIcon.map(({icon,href}, index) => (
                         <Grid key={index + "icon"} item xs={"auto"}>
-                            <IconButton className={cx(classes.linkStyle,"linkStyle")}>{icon}</IconButton>
+                            <IconButton target={"_blank"} href={href} className={cx(classes.linkStyle, "linkStyle")}>{icon}</IconButton>
                         </Grid>
                     ))}
 
