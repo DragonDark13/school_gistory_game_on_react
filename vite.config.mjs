6d51645674970fafd5b6a956afbe6e24c272a2ae
AleksandrZachkevich <aleksdark13@gmail.com>
2023-12-17 15:17:23 +0200

    config

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
     envPrefix:"REACT_ENV",
+    base: '/school_gistory_game_on_react/',
     // This changes the out put dir from dist to build
     // comment this out if that isn't relevant for your project
     build: {
