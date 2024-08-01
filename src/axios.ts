b9022a610e80a9ef35d4e2c5abfbbc3c3b6e18c0
Aleksandr <aleksdark13@gmail.com>
2024-08-01 08:09:50 +0300

    add url server

--- src/axios.ts
+++ src/axios.ts
@@ -2,26 +2,26 @@ import axios from 'axios';
 
 // Функція для визначення, чи є поточний сайт localhost
 const isLocalhost = () => {
-  // Перевіряємо, чи виконується код у браузері
-  if (typeof window !== 'undefined') {
-    return window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-  }
-  // Якщо не в браузері, а в середовищі Node.js, повертаємо false
-  return false;
+    // Перевіряємо, чи виконується код у браузері
+    if (typeof window !== 'undefined') {
+        return window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    }
+    // Якщо не в браузері, а в середовищі Node.js, повертаємо false
+    return false;
 };
 
 // Змінна для зберігання baseURL
-let baseURL = 'https://zelse.asuscomm.com/SchoolHistoryGame';
+let baseURL = 'https://aleksdark1313.pythonanywhere.com/';
 
 let remoteServer = false;
 
 // Якщо поточний сайт localhost, змінюємо baseURL
 if (isLocalhost() && !remoteServer) {
-  baseURL = 'http://127.0.0.1:5000/';
+    baseURL = 'http://127.0.0.1:5000/';
 }
 
 const axiosClient = axios.create({
-  baseURL: baseURL,
+    baseURL: baseURL,
 });
 
 export default axiosClient;
